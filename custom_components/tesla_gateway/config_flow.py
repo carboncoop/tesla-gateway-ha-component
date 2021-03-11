2036024c45807de877b5b8d41e79b8b7707580d2
Peter Armstrong <peter@carbon.coop>
2021-03-11 13:32:08 +0000

    config flow work

---
+++ custom_components/tesla_gateway/config_flow.py
@@ -0,0 +1,40 @@
+from typing import Any, Dict, Optional
+
+from homeassistant import config_entries
+from homeassistant.const import (
+    CONF_USERNAME,
+    CONF_PASSWORD,
+    CONF_ACCESS_TOKEN,
+)
+import homeassistant.helpers.config_validation as cv
+import voluptuous as vol
+
+from .const import DOMAIN, CONF_REFRESH_TOKEN
+
+
+AUTH_SCHEMA = vol.Schema(
+    {
+        vol.Required(CONF_USERNAME): cv.string,
+        vol.Optional(CONF_PASSWORD): cv.string,
+        vol.Optional(CONF_ACCESS_TOKEN, default=""): cv.string,
+        vol.Optional(CONF_REFRESH_TOKEN, default=""): cv.string,
+    }
+)
+
+
+class TeslaGatewayConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+    """Tesla Gateway config flow."""
+
+    data: Optional[Dict[str, Any]]
+
+    async def async_step_user(self, user_input: Optional[Dict[str, Any]] = None):
+        """Invoked when a user initiates a flow via the user interface."""
+        errors: Dict[str, str] = {}
+        if user_input is not None:
+            if not errors:
+                self.data = user_input
+                return self.async_create_entry(title="Tesla Gateway", data=self.data)
+
+        return self.async_show_form(
+            step_id="user", data_schema=AUTH_SCHEMA, errors=errors
+        )
--- custom_components/tesla_gateway/const.py
+++ custom_components/tesla_gateway/const.py
@@ -1,1 +1,2 @@
 DOMAIN = "tesla_gateway"
+CONF_REFRESH_TOKEN = "refresh_token"
